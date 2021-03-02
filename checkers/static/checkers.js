4e64d6a0ddd970c595d345e017a8a993b7d5f3b0
Pedro Marques <roque@blp-digital.com>
2021-03-02 11:55:41 +0100

    Display end of game message.

--- checkers/static/checkers.js
+++ checkers/static/checkers.js
@@ -2,11 +2,14 @@
 var currentBoard = undefined;
 var selectedCell = null;
 var currentPlayOptions = [];
+var gameRunning = false;
 
 function board_update_content(response) {
-    let xboard = document.querySelector("#board");
-    let tbody = xboard.querySelector("tbody");
+    // Update the board given the response from the webserver
 
+    // Create the state of all the board positions
+    // positions start as empty and are then populated from the server
+    // response which contains a list of triples of the form (row, col, piece)
     let board = new Array(8);
     for (i = 0; i < 8; i++) {
         board[i] = new Array(8).fill(0);
@@ -19,6 +22,12 @@ function board_update_content(response) {
     });
 
     currentBoard = board;
+
+    // Update the HTML elements. Table elements that have a piece are assigned
+    // html text and a color class; elements with no piece have this properties
+    // reset.
+    let xboard = document.querySelector("#board");
+    let tbody = xboard.querySelector("tbody");
 
     for (row = 0; row < 8; row++) {
         for (col = 0; col < 8; col++) {
@@ -45,16 +54,34 @@ function board_update_content(response) {
         }
     }
 
+    // Update the score box
     el_count_computer = document.querySelector("#computer-capture-count");
     let value = 12 - response['pieces'][1];
     el_count_computer.innerHTML = value.toString();
     el_count_player = document.querySelector("#player-capture-count");
     value = 12 - response['pieces'][0];
     el_count_player.innerHTML = value.toString();
+
+    // Update the game-over box.
+    gameRunning = response['pieces'].every(value => (value > 0));
+    let game_over_div = document.querySelector("div.game-over");
+    let win = game_over_div.querySelector("#winner");
+
+    if (gameRunning) {
+        game_over_div.style.display = "none";
+        win.innerHTML = "";
+    } else {
+        game_over_div.style.display = "block";
+        if (response['pieces'][0] == 0) {
+            win.innerHTML = "Player (black) wins";
+        } else {
+            win.innerHTML = "Computer (white) wins";
+        }
+    }
 }
 
 function board_update() {
-    const response = fetch('/api/board')
+    return fetch('/api/board')
         .then(response => response.json())
         .then(function (data) {
             board_update_content(data);
@@ -80,8 +107,10 @@ function showPlayOptions() {
 }
 
 function getPlayOptions(row, col) {
+    // When a user clicks on one of its pieces, fetch the possible moves and
+    // highlight them on the board
     let path = '/api/moves/' + row.toString() + '/' + col.toString();
-    const response = fetch(path)
+    return fetch(path)
         .then(response => response.json())
         .then(function (data) {
             currentPlayOptions = data;
@@ -90,6 +119,7 @@ function getPlayOptions(row, col) {
 }
 
 function clearPlayOptions(row, col) {
+    // Remove any position highlights
     let xboard = document.querySelector("#board");
     let tbody = xboard.querySelector("tbody");
 
@@ -119,7 +149,7 @@ function update_computer_move(response) {
         str += "(" + element[0] + ", " + chr + ")";
     });
 
-    element = document.querySelector('#last-move')
+    let element = document.querySelector('#last-move')
     element.innerHTML = str;
 }
 
@@ -139,7 +169,7 @@ function makeMove(start, end) {
         'end': end,
     };
 
-    fetch('/api/move', {
+    return fetch('/api/move', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -147,7 +177,7 @@ function makeMove(start, end) {
         body: JSON.stringify(request_data)
     }).then(
         function (response) {
-            board_update();
+            return board_update();
         }
     );
 }
@@ -168,32 +198,41 @@ function advanceGame() {
         .then(response => response.json())
         .then(function (data) {
             update_computer_move(data);
-            board_update();
+            return board_update();
         });
 
 }
 
 function onCellClick(element, event) {
+    // Handle a click on the checkers board
+    // Clicking on a piece selects that piece to move and displays potential
+    // move options; clicking on one of the move options triggers the move.
     const row = parseInt(element.getAttribute("row"));
     const col = parseInt(element.getAttribute("col"));
 
     let value = currentBoard[row][col];
     if (value == 0) {
-        if (selectedCell == null) {
+        if (selectedCell == null || !gameRunning) {
             return;
         }
         if (isInCurrentOptions(row, col)) {
-            makeMove(selectedCell, [row, col]);
+            let promise = makeMove(selectedCell, [row, col]);
             clearPlayOptions(selectedCell[0], selectedCell[1]);
             selectedCell = null;
-            advanceGame();
+            promise.then(function () {
+                if (gameRunning) {
+                    advanceGame();
+                }
+            });
         }
     } else if (value == 2 || value == 4) {
         if (selectedCell != null) {
             clearPlayOptions(selectedCell[0], selectedCell[1]);
         }
         selectedCell = [row, col];
-        getPlayOptions(row, col);
+        if (gameRunning) {
+            getPlayOptions(row, col);
+        }
     } else if (value == 1 || value == 3) {
         if (selectedCell != null) {
             clearPlayOptions(selectedCell[0], selectedCell[1]);
@@ -202,8 +241,29 @@ function onCellClick(element, event) {
     }
 }
 
-function initialize() {
+function game_restart() {
+    // Called when the user clicks on the restart button.
+    // Cleans up last-move and play-options and sends a restart
+    // request to the web server.
+    let element = document.querySelector('#last-move')
+    element.innerHTML = "";
 
+    if (selectedCell != null) {
+        clearPlayOptions(selectedCell[0], selectedCell[1]);
+        selectedCell = null;
+    }
+
+    fetch('/api/restart', { 'method': 'POST' })
+        .then((response) => {
+            if (response.status != 200) {
+                console.log(response);
+            }
+            board_update();
+        });
+};
+
+function initialize() {
+    // Register event listeners and fetch the board contents.
     board_update();
 
     let xboard = document.querySelector("#board");
@@ -217,9 +277,15 @@ function initialize() {
             td.setAttribute("col", col.toString());
             td.addEventListener("click", function (event) {
                 onCellClick(this, event);
-            })
+            });
         }
     }
+
+    let btn_restart = document.querySelector("#restart");
+    btn_restart.addEventListener("click", function (event) {
+        game_restart();
+    });
+
 }
 
 document.addEventListener("DOMContentLoaded", () => {
