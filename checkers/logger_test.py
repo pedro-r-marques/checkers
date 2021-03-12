2bb92f069efa3ab59c87da02a25e550f689e1696
Pedro Marques <pedro.r.marques@gmail.com>
2021-03-12 10:51:01 +0100

    Save binary logs and summarize them from disk.

--- checkers/logger_test.py
+++ checkers/logger_test.py
@@ -1,8 +1,11 @@
 import unittest
 import tempfile
+import shutil
+import os
 
-from .logger import GameLogger
+from .logger import GameLogger, SummaryLogger
 from .py_checkers import PyCheckersBoard as CheckersBoard
+from .play_random import move_select
 
 
 class GameLoggerTest(unittest.TestCase):
@@ -17,3 +20,33 @@ class GameLoggerTest(unittest.TestCase):
         reader = GameLogger()
         reader.load(filename)
         self.assertEqual(len(reader.history), 2)
+
+
+def log_mock_game(filename):
+    logger = GameLogger()
+    board = CheckersBoard()
+    for turn in range(64):
+        player = CheckersBoard.BLACK if turn % 2 == 0 else CheckersBoard.WHITE
+        mv = move_select(board, player, turn)
+        logger.log(board, turn, player, mv)
+        if mv is not None:
+            board.move(mv)
+        if any(x == 0 for x in board.count()):
+            break
+
+    logger.save(filename)
+
+
+class SummaryLoggerTest(unittest.TestCase):
+    def setUp(self):
+        self.tmpdir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.tmpdir)
+
+    def test_aggregate(self):
+        for filename in ['a.dat', 'b.dat']:
+            log_mock_game(os.path.join(self.tmpdir, filename))
+        summary = SummaryLogger()
+        summary.from_directory(self.tmpdir, r'.*\.dat')
+        summary.save(os.path.join(self.tmpdir, 'summary.tsv'), 0)
