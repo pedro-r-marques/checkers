67f41f65396ef896fd2bf20419fb1ac5fde8b67b
Pedro Marques <pedro.r.marques@gmail.com>
2021-03-12 11:17:43 +0100

    Update logging code (#5)
    * Save binary logs and summarize them from disk.
    
    * Adapt the auto-play script to new logger code.

--- checkers/logger.py
+++ checkers/logger.py
@@ -1,6 +1,10 @@
 import collections
 import pickle
+import os
+import re
+import warnings
 
+from .py_checkers import PyCheckersBoard as CheckersBoard
 
 LogEntry = collections.namedtuple(
     'LogEntry', ['board', 'turn', 'player', 'move'])
@@ -21,6 +25,25 @@ class GameLogger():
         with open(filename, 'rb') as fp:
             self.history = pickle.load(fp)
 
+    @staticmethod
+    def _board_object(log_entry):
+        board = CheckersBoard()
+        state = [[] for _ in range(8)]
+        for i in range(8):
+            start = i * 8
+            state[i] = [int(x) for x in log_entry.board[start: start + 8]]
+        board.initialize(state)
+        return board
+
+    def player_counts(self, index):
+        """ Return the number of pieces per player after turn X.
+        """
+        log_entry = self.history[index]
+        board = self._board_object(log_entry)
+        if log_entry.move is not None:
+            board.move(log_entry.move)
+        return board.count()
+
 
 class SummaryLogger():
     def __init__(self):
@@ -28,31 +51,49 @@ class SummaryLogger():
 
     @classmethod
     def hash(cls, log_entry):
-        board_arr, _, player, _ = log_entry
-        return hash((board_arr, player))
+        return hash((log_entry.board, log_entry.player))
 
-    def add(self, game_log, winner, turns):
+    def add(self, game_log, winner):
+        turns = len(game_log.history)
+
         for log_entry in game_log.history:
-            board_arr, turn, player, move = log_entry
-            turn_distance = turns - turn
+            turn_distance = turns - log_entry.turn
             h = self.hash(log_entry)
             if h not in self.data:
-                results = [(move, [(winner, turn_distance)])]
-                self.data[h] = (board_arr, player, results)
+                results = [(log_entry.move, [(winner, turn_distance)])]
+                self.data[h] = (log_entry.board, log_entry.player, results)
                 continue
             current = self.data[h]
-            if current[0] != board_arr or current[1] != player:
-                print('hash collision')
+            if current[0] != log_entry.board or current[1] != log_entry.player:
+                warnings.warn('hash collision')
                 continue
 
             exists = False
             for entry in current[2]:
-                if entry[0] == move:
+                if entry[0] == log_entry.move:
                     entry[1].append((winner, turn_distance))
                     exists = True
                     break
             if not exists:
-                current[2].append((move, [(winner, turn_distance)]))
+                current[2].append((log_entry.move, [(winner, turn_distance)]))
+
+    def from_directory(self, dirname, pattern=None, min_turns=48):
+        filenames = os.listdir(dirname)
+        if pattern is not None:
+            re_pattern = re.compile(pattern)
+            filenames = [f for f in filenames if re_pattern.match(f)]
+        for basename in filenames:
+            log = GameLogger()
+            log.load(os.path.join(dirname, basename))
+            counts = log.player_counts(-1)
+            winner = 0
+            if counts[0] == 0:
+                winner = CheckersBoard.BLACK
+            elif counts[1] == 0:
+                winner = CheckersBoard.WHITE
+            if winner == 0 and len(log.history) < min_turns:
+                continue
+            self.add(log, winner)
 
     def save(self, filename, threshold):
         with open(filename, 'w') as fp:
@@ -61,9 +102,9 @@ class SummaryLogger():
                 move_data = []
                 count = 0
                 for move, results in move_info:
-                    counts = [0, 0]
+                    counts = [0, 0, 0]
                     for result in results:
-                        counts[result[0] - 1] += 1
+                        counts[result[0]] += 1
                     move_data.append((move, counts))
                     count += sum(counts)
                 if count < threshold:
